83911278417b4a129e346fa6c19a24b44b2b17d0
yuyang1 <yuyang1@chinaums.com>
2018-06-04 20:06:28 +0800

    test

---
+++ src/Test2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yangyu on 2018/6/4.
+ */
+public class Test2 {
+}
---
+++ src/Test3.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yangyu on 2018/6/4.
+ */
+public class Test3 {
+    public static void main(String[] args) {
+
+    }
+}
