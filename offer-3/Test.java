17f5f5aceae390efe9d439d6b33be45c01a37644
yuyang1 <yuyang1@chinaums.com>
2018-06-11 14:47:04 +0800

    binaraytree

--- offer-3/Test.java
+++ offer-3/Test.java
@@ -4,11 +4,7 @@ public class Test {
 		int bb;
 	}
 	public static void main(String[] args) {
-		Test a = new Test();
-		a.aa = 1;
-		B b = a.new B();
-		b.bb = 2;
-		System.out.println(a.aa);
-		System.out.println(b.bb);
+		int[] preOrder;
+		System.out.println(null == preOrder);
 	}
 }
--- offer-4/Solution.java
+++ offer-4/Solution.java
@@ -1,22 +1,65 @@
 public class Solution {
-		/**
-	type	:	树
-	title	:	重建二叉树
-	desc	:	输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
-				假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
-				例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历
-				序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
-					A
-				B       C
-			D     E	  F
+		
 
-			前序遍历：访问根节点，前序遍历左子树，后序遍历右子树
-			中序遍历：中序遍历左子树，访问根节点，中序遍历右子树
-			后序遍历：后序遍历左子树，后续遍历右子树，访问根节点
-	前序遍历结果：ABDECF
-	中序遍历结果：DBEAFC
-	后序遍历结果：DEBFCA
-		**/	
+static class BinaryTreeNode {
+	int value;
+	BinaryTreeNode left;
+	BinaryTreeNode right;
+}
+	public static BinaryTreeNode construct(int[] preOrder, int[] inOrder) {
+		if (null == preOrder || null == inOrder || preOrder.length != inOrder.length) {
+			return null;
+		}
+		return construct(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length - 1);
+	}
+	private static BinaryTreeNode construct(int[] preOrder, int ps, int pe, int[] inOrder, int is, int ie) {
+		
+        if (ps > pe) {
+            return null;
+        }
+        
 
-	public static void main(String[] args){}
+		int val = preOrder[ps];
+		int index = is;
+
+		//查找根节点在中序遍历结果中的位置
+		while (index < ie && val != inOrder[index]) {index++;}
+		BinaryTreeNode node = new BinaryTreeNode();
+		node.value = val;
+		node.left = construct(preOrder, ps + 1, ps + index, inOrder, is, index - 1);
+		node.right = construct(preOrder, ps + index -is + 1, pe, inOrder, index + 1, ie);
+		return node;
+	}
+
+
+	public static void main(String[] args) {
+		int[] preOrder = {1, 2, 4, 7, 3, 5, 6, 8};
+		int[] inOrder = {4, 7, 2, 1, 5, 3, 8, 6};
+		BinaryTreeNode node = construct(preOrder, inOrder);
+		preOrderTree(node);
+		System.out.println("======");
+		inOrderTree(node);
+	}
+
+	public static void preOrderTree(BinaryTreeNode node) {
+		if (node != null) {
+			System.out.println(node.value);
+			preOrderTree(node.left);
+			preOrderTree(node.right);
+		}
+	}
+	// public static int[] inOrderTree(BinaryTreeNode node) {}
+	public static void inOrderTree(BinaryTreeNode node) {
+		if (node != null) {
+			inOrderTree(node.left);
+			System.out.println(node.value);
+			inOrderTree(node.right);
+		}
+	}
+
+
+
+
+	
+		
 }
